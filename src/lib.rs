0c0d4ad1280a27707510bbd9dfc62b4a54ddb06b
sonmezonur <onursonmez6116@gmail.com>
2018-08-18 03:32:06 +0300

    Organize lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#[macro_use]
+extern crate failure;
+
+
+extern crate image;
+extern crate rusttype;
+
+use rusttype::{point, Font, Scale};
+use image::{DynamicImage, Rgba};
+use failure::Fail;
+
+pub mod hex;
+pub mod avatar;
+
+pub use avatar::AvatarBuilder;
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-pub mod color; // content of network.rs
-
-fn main() {
-    let color_map = color::build_colors();
-    println!("type : {:?}", color_map);
-    println!("Default avatar color: {}", color_map.default)
-
-}
